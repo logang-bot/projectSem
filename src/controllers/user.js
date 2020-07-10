beb7c6e272ed063569ddd12e18a880477b94dbd6
erika <erikanoemychuya@gmail.com>
2020-07-09 21:51:56 -0400

    arreglos al controlador signup de user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -11,19 +11,28 @@ ctrl.index = async (req,res)=>{
 ctrl.signUp = async (req,res)=> {
     var usuario= new user
     //console.log(req.body)
-    var {name,email,password} = req.body 
+    var {name,email,password,confirm_password} = req.body 
 
-    if(name){  
+    if(name && email && password){  
+        const useremail= await user.findOne({email: email}) //no olvidar el await 
+        console.log(useremail)
+        if (useremail)
+           return res.status(400).json({message: "el email ya esta en uso, ingrese otro diferente"})
+        if(password != confirm_password)
+            return res.status(400).json({message: "las constraseñas no coinciden :/"})
         usuario.name = name;
         usuario.email = email;
-        usuario.password = await usuario.encrypt(password);    
+        usuario.password = await usuario.encrypt(password); 
         console.log(usuario)
         await usuario.save((err, usersaved) => {
             if(err) return res.status(500).send({message: `Error en el servidor ${err}`});
             if(usersaved){
                 const token = jwt.sign({id: usersaved._id}, config.secret,{
-                    expiresIn: 60 * 60 * 60
+                    expiresIn: 60 * 60 * 24
                 })
+                const decod = jwt.verify(token, config.secret)
+                req.userId = decod.id
+
                 return res.status(200).send({
                 usuario: usersaved,
                 token: token 
@@ -37,19 +46,22 @@ ctrl.signUp = async (req,res)=> {
         });
     }else{
         return res.status(400).send({
-            message: 'El nombre es requerido'
+            message: 'uno o mas campos estan vacios'
         });
-    }*/
+    }
 }
 ctrl.login = async (req,res)=>{
     const {email, password} = req.body
     const userr = await user.findOne({email:email})
     if (!user) return res.send('el usuario no esta registrado')
     const pass = userr.match(password)
-    if(!pass) return res.send('la contrasenia es incorrecta')
+    if(!pass) return res.send('la contraseña es incorrecta')
     const token = jwt.sign({id: userr._id}, config.secret,{
         expiresIn: 60 * 60 * 60
     })
+    const decod = jwt.verify(token, config.secret)
+    req.userId = decod.id
+
     res.send({
         message: "estas logueado",
         token: token
